86536dc8606f713983ed99fe56e2228017bb490d
김건우 <201944085p@gmail.com>
2024-08-26 20:08:04 +0900

    springboot 추가

---
+++ backend/springboot/src/main/java/com/inhatc/flightmore/FlightmoreApplication.java
@@ -0,0 +1,12 @@
+package com.inhatc.flightmore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightmoreApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FlightmoreApplication.class, args);
+	}
+
+}
