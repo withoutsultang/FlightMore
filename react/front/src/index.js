87c895721d0f54d5085bef8647b337fd300b04ab
김건우 <201944085p@gmail.com>
2024-08-26 17:04:23 +0900

    Main 이름 변경 / CSS 수정

--- react/front/src/index.js
+++ react/front/src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
-import Main from './ui/main';
+import Main from './ui/Main';
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
