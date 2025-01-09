20a761abaf75f76bfdc5154413b6df81df0726be
김건우 <202447009@itc.ac.kr>
2025-01-09 16:37:47 +0900

    v1

--- python/naver_flight.py
+++ python/naver_flight.py
@@ -1,4 +1,6 @@
-# 데이터베이스에 저장할 때 칼럼 네임 통일이랑 어디서 수집햇는지, 언제 수집햇는지  칼럼 추가
+from http.client import responses
+
+import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
@@ -7,14 +9,51 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import time
 import logging
-from dbconn import Database
 from datetime import datetime, timedelta
 from concurrent.futures import ThreadPoolExecutor
-
+import json
 
 # 로깅 설정
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+SPRING_BOOT_URL = "http://localhost:8080/api"
+
+def is_duplicate(data, all_flight_data):
+    for flight in all_flight_data:
+        if (flight["airline"] == data["airline"] and
+            flight["arrival_airport"] == data["arrival_airport"] and
+            flight["arrival_time"] == data["arrival_time"] and
+            flight["departure_airport"] == data["departure_airport"] and
+            flight["departure_time"] == data["departure_time"]):
+            return True
+    return False
+
+def fetch_notification():
+    response = requests.get(f"{SPRING_BOOT_URL}/notify/all")
+    if response.status_code == 200:
+        return response.json()
+    else:
+        logging.error(f"알림 조회 실패: {response.status_code}{response.text}")
+        return []
+
+def send_data(flight_data):
+    flight_data = [data for data in flight_data if data is not None]
+
+    if not flight_data:
+        logging.error("전송할 데이터가 없습니다. 데이터가 None이거나 잘못된 형식입니다.")
+        return
+    url  = f"{SPRING_BOOT_URL}/flights"
+    headers = {
+        'Content-Type': 'application/json',
+    }
+    try:
+        response = requests.post(url, headers=headers, data=json.dumps(flight_data))
+        if response.status_code == 200:
+            logging.info(f"데이터 전송 성공: {response.json()}")
+        else:
+            logging.error(f"데이터 전송 실패: {response.status_code}{response.text}")
+    except Exception as e:
+        logging.error(f"데이터 전송 중 오류 발생: {e}")
 
 def init_webdriver():
     option = webdriver.ChromeOptions()
@@ -22,8 +61,10 @@ def init_webdriver():
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=option)
     return driver
 
+
 def wait_for_element(driver, by, value, timeout=30):
     return WebDriverWait(driver, timeout).until(EC.presence_of_element_located((by, value)))
+
 
 def scroll_to_bottom(driver):
     last_height = driver.execute_script("return document.body.scrollHeight")
@@ -35,100 +76,116 @@ def scroll_to_bottom(driver):
             break
         last_height = new_height
 
-def extract_flight_data(flight, departure_date):
-    try:  # 수정: try-except 블록 추가로 예외 처리 강화
+
+def extract_flight_data(flight, departure_date, arrival_date, url):
+    try:
+        # 항공사 이름 추출
         flight_airline = flight.find_element(By.CSS_SELECTOR, '.airline_name__0Tw5w').text.strip()
+
+        # 출발/도착 시간 및 공항 정보 추출
         departure_time = flight.find_elements(By.CSS_SELECTOR, '.route_time__xWu7a')[0].text.strip()
         departure_airport = flight.find_elements(By.CSS_SELECTOR, '.route_code__S07WE')[0].text.strip()
         arrival_time = flight.find_elements(By.CSS_SELECTOR, '.route_time__xWu7a')[1].text.strip()
         arrival_airport = flight.find_elements(By.CSS_SELECTOR, '.route_code__S07WE')[1].text.strip()
-        flight_duration = flight.find_element(By.CSS_SELECTOR, '.route_info__RVh13').text.strip()
-        flight_duration_text = flight_duration.replace('직항,', '').strip() if '직항,' in flight_duration else flight_duration
 
+         # 가격 정보 추출
         try:
-            price = int(flight.find_element(By.CSS_SELECTOR, '.item_promoted__fRqOW .item_num__aKbk4').text.strip().replace(",", "").replace("원", ""))
+            price_element = flight.find_element(By.CSS_SELECTOR, '.item_usual__wg002 .item_num__aKbk4')
         except:
-            price = int(flight.find_element(By.CSS_SELECTOR, '.item_usual__wg002 .item_num__aKbk4').text.strip().replace(",", "").replace("원", ""))
+            price_element = flight.find_element(By.CSS_SELECTOR, '.item_firstItem__TFXDv .item_num__aKbk4')
 
-        flight_arrival_time = datetime.strptime(f"{departure_date} {arrival_time}", '%Y%m%d %H:%M')
-        flight_departure_date = datetime.strptime(f"{departure_date} {departure_time}", '%Y%m%d %H:%M')
-        hours, minutes = map(int, flight_duration_text.replace('시간', '').replace('분', '').split())
-        flight_time = timedelta(hours=hours, minutes=minutes)
+        price = int(price_element.text.strip().replace(",", "").replace("원", ""))
 
+
+        # 출발 및 도착 시간 포맷팅
+        flight_arrival_time = datetime.strptime(f"{arrival_date} {arrival_time}", '%Y%m%d %H:%M').isoformat()
+        flight_departure_date = datetime.strptime(f"{departure_date} {departure_time}", '%Y%m%d %H:%M').isoformat()
+
+        # 데이터 구성
         return {
-            "flight_number": "",
-            "airline": flight_airline,
-            "arrival_airport": arrival_airport,
-            "arrival_time": flight_arrival_time,
-            "departure_airport": departure_airport,
-            "departure_time": flight_departure_date,
-            "flight_time": flight_time,
-            "price": price,
-            "site": "Naver",
-            "created_date": datetime.now(),
-            "updated_date": None
+                "flightNumber": "",
+                "flightAirline": flight_airline,
+                "flightArrival": arrival_airport,
+                "flightArrivalDate": flight_arrival_time,
+                "flightDeparture": departure_airport,
+                "flightDepartureDate": flight_departure_date,
+                "flightPrice": price,
+                "flightSite": "Naver",
+                "url": url,
+                "createdDate": datetime.now().isoformat(),
+                "updatedDate": datetime.now().isoformat()
         }
     except Exception as e:
-        logging.error(f"데이터 추출 중 오류 발생: {e}")  # 수정: print 대신 logging 사용
+        logging.error(f"데이터 추출 중 오류 발생: {e}. 항공편: {flight.text[:100]}")  # 오류 위치를 쉽게 파악할 수 있도록 추가 정보 제공
         return None
 
 
 
-def crawl_flight_data(driver, url, departure_date, db):
-    driver = init_webdriver()
+def crawl_flight_data(url, departure_date, arrival_date, driver):
     try:
         driver.get(url)
+        driver.implicitly_wait(2)
         wait_for_element(driver, By.CSS_SELECTOR, '#container > div.priceGraph_PriceGraph__HsePr')
         scroll_to_bottom(driver)
         flights = driver.find_elements(By.CSS_SELECTOR, '.concurrent_ConcurrentItemContainer__NDJda')
 
         flight_data = []
         for flight in flights:
-            data = extract_flight_data(flight, departure_date)
-            if data and not db.is_duplicate(data["airline"], data["arrival_airport"], data["arrival_time"], data["departure_airport"], data["departure_time"]):
-                flight_data.append(data)
+            data = extract_flight_data(flight, departure_date, arrival_date, url)
+
+            flight_data.append(data)
 
         return flight_data
     except Exception as e:
-        logging.error(f"크롤링 중 오류 발생: {e}")  # 수정: print 대신 logging 사용
+        logging.error(f"크롤링 중 오류 발생: {e}. URL: {url}")
         return []
-    finally:
-        driver.quit()
+
 
 def main():
     start_time = time.time()
 
-    db = Database()
+    notifications = fetch_notification() # Spring boot로부터 알림 조회
+    if not notifications:
+        logging.info("조회된 알림이 없습니다.")
+        return
 
-    departures = 'ICN'
-    arrivals = ['TYO', 'KIX', 'NGO', 'FUK', 'CTS']  # 여러 도착지 설정
-    departure_date = '20240918' # 추후 수정
-    arrival_date = '20240920' # 추후 수정
-    adult = 1
-    child = 0
+    driver = init_webdriver()  # 드라이버를 한 번만 생성
+
+    all_flight_data = []
 
     try:
-        # 수정: ThreadPoolExecutor를 사용한 병렬 처리
         with ThreadPoolExecutor(max_workers=5) as executor:
             futures = []
-            for arrival in arrivals:
+            for notification in notifications:
+                departures = notification["departureLocation"]
+                arrival = notification["arrivalLocation"]
+                departure_date = notification["departureDate"].replace("-", "")
+                arrival_date = notification["arrivalDate"].replace("-", "")
+                adult = notification["numPeople"]
+
                 url = (f"https://flight.naver.com/flights/international/"
                        f"{departures}-{arrival}-{departure_date}/{arrival}-{departures}-{arrival_date}"
-                       f"?adult={adult}&child={child}&isDirect=true&fareType=Y")
-                futures.append(executor.submit(crawl_flight_data, url, departure_date, db))
+                       f"?adult={adult}&isDirect=true&fareType=Y")
+                futures.append(executor.submit(crawl_flight_data, url, departure_date, arrival_date, driver))
 
             for future in futures:
                 flight_data = future.result()
                 if flight_data:
-                    db.insert_flight_data_batch(flight_data)
+                    all_flight_data.extend(flight_data)
+        if all_flight_data:
+            logging.info(f"총 수집된 항공 데이터 수: {len(all_flight_data)}")
+            print(all_flight_data[1])
+            send_data(all_flight_data)
+        else:
+            logging.info("수집된 항공 데이터가 없습니다.")
 
     except Exception as e:
-        logging.error(f"메인 실행 중 오류 발생: {e}")  # 수정: print 대신 logging 사용
+        logging.error(f"메인 실행 중 오류 발생: {e}")
     finally:
-        db.close()
+        driver.quit()
         end_time = time.time()
         total_time = end_time - start_time
-        logging.info(f"네이버 항공권 크롤링 완료. 소요 시간: {total_time:.2f}초")  # 수정: print 대신 logging 사용
+        logging.info(f"네이버 항공권 크롤링 완료. 소요 시간: {total_time:.2f}초")
 
 
 if __name__ == "__main__":
